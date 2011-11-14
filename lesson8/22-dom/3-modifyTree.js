e6d63b2925f6a2cc64df2b4fe08eebc0ea3ce8ce
Mikhail Baranov <mikhail.baranov@gmail.com>
2011-11-14 23:55:25 +0400

    Fixed grammar bugs

--- lesson8/22-dom/3-modifyTree.js
+++ lesson8/22-dom/3-modifyTree.js
@@ -1,8 +1,8 @@
 function modify() {
-	var newElem = document.createElement("P")
-	newElem.id = "newP"
-	var newText = document.createTextNode("Это второй параграф.")
-	newElem.appendChild(newText)
-	document.body.appendChild(newElem)
-// document.getElementById("emphasis1").childNodes[0].nodeValue = "первый "
+	var newElem = document.createElement("P");
+	newElem.id = "newP";
+	var newText = document.createTextNode("Это второй параграф.");
+	newElem.appendChild(newText);
+    document.body.appendChild(newElem);
+    document.getElementById("emphasis1").childNodes[0].nodeValue = "первый "
 }
